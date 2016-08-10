31dd334db949289be0a2e9a13eb72ebd351ede61
LHM0428 <lhm3031@hanmail.net>
2016-08-10 14:46:07 +0900

    2 Commit HM  - login btn

--- app/src/main/java/nhm/com/odiya/main/MainActivity.java
+++ app/src/main/java/nhm/com/odiya/main/MainActivity.java
@@ -1,9 +1,12 @@
 package nhm.com.odiya.main;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 import nhm.com.odiya.R;
+import nhm.com.odiya.login.LoginActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +14,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_activity);
+    }
+
+    public void loginClicked(View view){
+
+        Intent intent = new Intent(this, LoginActivity.class);
+        intent.putExtra("하아아이",true);
+        this.startActivity(intent);
     }
 }
