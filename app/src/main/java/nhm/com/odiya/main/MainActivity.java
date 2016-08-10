941758d4945ec017b3719e9854e49a726f2ada41
LHM0428 <lhm3031@hanmail.net>
2016-08-10 15:30:12 +0900

    2 Commit HM  - login btn

--- app/src/main/java/nhm/com/odiya/main/MainActivity.java
+++ app/src/main/java/nhm/com/odiya/main/MainActivity.java
@@ -4,16 +4,62 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TableLayout;
+import android.widget.ViewFlipper;
 
 import nhm.com.odiya.R;
 import nhm.com.odiya.login.LoginActivity;
 
 public class MainActivity extends AppCompatActivity {
 
+    ViewFlipper vf_event;
+    TableLayout tb_coupon;
+    Button bt_coupon;
+    boolean IsClosed = true;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_activity);
+
+        vf_event = (ViewFlipper) findViewById(R.id.vf_event);
+        System.out.println("vf_event : "+vf_event);
+        //Java 코드로도 ViewFlipper에 image를 넣을 수 있다.
+        ImageView img = new ImageView(this);
+        img.setImageResource(R.drawable.event_3);
+        vf_event.addView(img,2);
+
+        //ViewFlipper의 이미지 변경 Animation을 설정
+        // [ 왼쪽으로 슬라이딩 되며 들어온다 ]
+        Animation showIn = AnimationUtils.loadAnimation(this, android.R.anim.slide_in_left);
+        vf_event.setInAnimation(showIn);
+        // [ 오른쪽으로 슬라이딩 되며 퇴장한다 ]
+        vf_event.setOutAnimation(this, android.R.anim.slide_out_right);
+
+        // 1.5초 간격으로 ViewFilpper의 view를 자동 교체
+        vf_event.setFlipInterval(1500);
+        vf_event.startFlipping();
+
+        tb_coupon = (TableLayout) findViewById(R.id.tb_coupon);
+        bt_coupon = (Button) findViewById(R.id.bt_coupon);
+        tb_coupon.setVisibility(View.INVISIBLE);
+
+        bt_coupon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(IsClosed){
+                    tb_coupon.setVisibility(View.VISIBLE);
+                    IsClosed = false;
+                }else{
+                    tb_coupon.setVisibility(View.INVISIBLE);
+                    IsClosed = true;
+                }
+            }
+        });
     }
 
     public void loginClicked(View view){
